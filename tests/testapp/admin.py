ca0e86b08e4d919881a747074d58d88b09d27aa2
Pascal F <pfouque@users.noreply.github.com>
2025-06-03 18:04:33 +0200

    Add admin

---
+++ tests/testapp/admin.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from django.contrib import admin
+
+from django_fsm.admin import FSMAdminMixin
+
+from .models import AdminBlogPost
+
+
+@admin.register(AdminBlogPost)
+class AdminBlogPostAdmin(FSMAdminMixin, admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "state",
+        "step",
+    )
+
+    fsm_fields = [
+        "state",
+        "step",
+    ]
