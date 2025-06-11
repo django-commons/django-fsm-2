28d42576182d5a636a4ccd074d4fb6097cb79968
Pascal Fouque <pfouque@users.noreply.github.com>
2025-06-11 16:29:53 +0200

    Update test apps according to Django 4.2 template (#50)

--- tests/manage.py
+++ tests/manage.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+
 from __future__ import annotations
 
 import os
 import sys
 
-from django.core.management import execute_from_command_line
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-sys.path.insert(0, PROJECT_ROOT)
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        sys.argv += ["test"]
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    execute_from_command_line(sys.argv)
+    main()
