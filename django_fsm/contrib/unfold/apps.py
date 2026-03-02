b9c573cabd3c2ce9b7eb2fe6c1e0f45e5af21d5b
pfouque <pfouque@users.noreply.github.com>
2026-03-02 10:10:26 +0100

    Add Unfold contrib

---
+++ django_fsm/contrib/unfold/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class UnfoldFsmConfig(AppConfig):
+    name = "django_fsm.contrib.unfold"
+    label = "unfold_fsm"
--- tests/settings.py
+++ tests/settings.py
@@ -47,6 +47,8 @@ PROJECT_APPS = (
 )
 
 INSTALLED_APPS = [
+    "unfold",  # before django.contrib.admin
+    "django_fsm.contrib.unfold",  # optional, if django-fsm-2 package is used
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
